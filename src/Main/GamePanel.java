8dc9078da432038942d33b45160d93c4228493ce
Jenny <jenniferduong20012004@gmail.com>
2024-02-02 00:38:20 +0700

    Import picture

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -15,21 +15,31 @@ public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     public GamePanel(){
         mouseInputs = new MouseInputs(this);
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
         importImg();
     }
-    public void paintComponent (Graphics g){
 
+    private void setPanelSize() {
+        Dimension size = new Dimension(3120, 1450);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMinimumSize(size);
+    }
+
+    public void paintComponent (Graphics g){
+        super.paintComponent(g);
+        g.drawImage(img.getSubimage(0,0,78,58),0,0,156,116,null);
     }
 
     private void importImg() {
-        /*InputStream is = getClass().getResourceAsStream("Res/Main-char/Attack (78x58).png");
+        InputStream is = getClass().getResourceAsStream("/Player/Attack (78x58).png");
         try {
             img = ImageIO.read(is);
         } catch (IOException e){
             e.printStackTrace();
-        }*/
+        }
     }
 }
--- src/Main/GameWindow.java
+++ src/Main/GameWindow.java
@@ -4,9 +4,11 @@ import javax.swing.*;
 
 public class GameWindow extends JFrame{
     public GameWindow(GamePanel gamePanel){
-        setSize(400,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(gamePanel);
+        //setLocationRelativeTo(null);
+        setResizable(false);
+        pack();
         setVisible(true);
 
     }
