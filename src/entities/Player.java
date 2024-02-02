4be9cd8a68d4102e44c49212480992272022b83c
Jenny <jenniferduong20012004@gmail.com>
2024-02-02 17:59:08 +0700

    Add entity

---
+++ src/entities/Player.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Player extends Entity{
+
+    public Player(float x, float y) {
+        super(x, y);
+    }
+    public void update (){
+        //observer
+    }
+    public void render(){
+
+    }
+}
