1bd30f7e10e3f2bcd9a9c705a5927c2517b0f722
jenny <jenniferduong20012004@gmail.com>
2024-02-09 18:21:57 +0700

    add npig

--- src/entities/Pig.java
+++ src/entities/Pig.java
@@ -1,4 +1,9 @@
 package entities;
 
-public class Pig {
+import static utilz.Constant.EnemyConstants.*;
+
+public class Pig extends Enemy{
+    public Pig(float x, float y) {
+        super(x, y, PIG_WIDTH, PIG_HEIGHT, PIG);
+    }
 }
