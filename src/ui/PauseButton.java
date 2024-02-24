69c4dcc96752f449ee9bb8afeb484a55961e2993
jenny <jenniferduong20012004@gmail.com>
2024-02-24 11:44:33 +0700

    add sound

--- src/ui/PauseButton.java
+++ src/ui/PauseButton.java
@@ -1,4 +1,37 @@
 package ui;
 
-public class PauseButton {
+import java.awt.*;
+
+public abstract class PauseButton implements Buttons{
+    protected int x,y,width, height;
+    protected Rectangle bounds;
+    public PauseButton(int x, int y, int width, int height){
+        this.x = x;
+        this. y = y;
+        this.width = width;
+        this.height = height;
+        createBounds();
+    }
+
+    private void createBounds() { bounds = new Rectangle(x,y,width, height);}
+
+    @Override
+    public void loadImgs() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void initBound() {
+
+    }
 }
---
+++ src/ui/SoundButton.java
@@ -0,0 +1,7 @@
+package ui;
+
+public class SoundButton extends PauseButton{
+    public SoundButton(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+}
